f03130cacdc28d0ec2b12fe44175d881d17bc8b2
soham_chogale <chogalesoham74@gmail.com>
2025-06-04 20:19:41 +0530

    fex/change port no

--- index.js
+++ index.js
@@ -12,5 +12,5 @@ app.use(cors());
 app.use(express.json());
 app.use("/api", schoolRoutes);
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 8080;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
